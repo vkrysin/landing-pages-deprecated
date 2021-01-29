43c695ad8097fc4491c7b260eae9e843200050cb
Vladislav Krysin <krysin18@mail.ru>
2021-01-29 20:55:53 +0300

    fix bag with img in final index.html (add img folder with the help of copy plugin in dist folder)

--- AliDesign/webpack.config.js
+++ AliDesign/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
@@ -42,6 +43,11 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "img", to: "img" },
+      ],
+    }),
   ],
 
   output: {
